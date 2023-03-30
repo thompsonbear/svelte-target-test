e4fd86f6e82e0459490a308a00936e16692f9ccb
Thompson Bear <thompsonbear21@gmail.com>
2023-03-30 14:44:10 -0400

    init commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<nav>
+    <a href="/test#square">Link to '/test#square'</a>
+<a href="/">Link to Home</a>
+</nav>
+
+<slot/>
+
+<style>
+    nav{
+        display: flex;
+        flex-direction: column;
+    }
+
+    a{
+        padding: 4px;
+        text-decoration: none;
+        color: black;
+        border: 1px solid black;
+        width: min-content;
+        white-space: nowrap;
+        border-radius: 8px;
+        margin: 4px;
+    }
+
+    a:hover{
+        background: #e0e0e0;
+    }
+</style>
---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,21 @@
+<div class="square" id="square">
+    <p>This square turns red if navigating to '/test#square' from the current page but not when navigating from another page.</p>
+</div>
+
+<style>
+    p{
+        color: white;
+        font-family: sans-serif;
+        padding: 2rem;
+    }
+    .square{
+        margin: 20px;
+        background: black;
+        height: 300px;
+        width: 300px;
+    }
+
+    .square:target{
+        background: red;
+    }
+</style>
